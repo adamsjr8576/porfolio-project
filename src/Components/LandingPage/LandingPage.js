8f4a32ae10281ce4d21c82b5000eee612e246d3a
John Adams <adamsjr8576@gmail.com>
2020-03-21 09:58:14 -0600

    Get timing for triangle animation complete

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './LandingPage.scss';
 import { images } from '../../images/images.js';
-
+// <img className="app-landing-logo" src={images.squareLogo} />
 export const LandingPage = () => {
 
   return (
     <main className="App">
       <div className="app-landing-page" >
         <h1 className="app-landing-name"><span>John</span> <span>Adams</span></h1>
-        <img className="app-landing-logo" src={images.triangleLogo} />
+        <div className="triangle-container">
+          <img className="app-landing-logo1" src={images.triangle} />
+          <img className="app-landing-logo2" src={images.triangle} />
+          <img className="app-landing-logo3" src={images.triangle} />
+          <img className="app-landing-logo4" src={images.triangle} />
+          <img className="app-landing-logo5" src={images.triangle} />
+          <img className="app-landing-logo6" src={images.triangle} />
+          <img className="app-landing-logo7" src={images.triangle} />
+        </div>
         <h3 className="app-landing-title">Software Developer</h3>
         <button className="app-landing-button">down arrow</button>
       </div>
--- src/images/images.js
+++ src/images/images.js
@@ -1,9 +1,13 @@
 import triangleLogo from './impossibletriangle.png';
 import oceanBackground from './oceantexture2.jpg';
+import squareLogo from './Square.png';
+import triangle from './triangle.png';
 
 export const images = {
   triangleLogo,
-  oceanBackground
+  oceanBackground,
+  squareLogo,
+  triangle
 }
 
 export default images
